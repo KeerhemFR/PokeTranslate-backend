8cd2115f3ca56fbd6c82fa419012b91af1adc80d
KeerhemFR <keerhem.guillaume@hotmail.fr>
2022-02-03 16:37:11 +0100

    Added the routes to call

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-const connection = require('./conf');
+const pokemonsRoutes = require('./routes/pokemons');
+const movesRoutes = require('./routes/moves');
+const abilitiesRoutes = require('./routes/abilities');
+const itemsRoutes = require('./routes/items');
+const typesRoutes = require('./routes/types');
+const naturesRoutes = require('./routes/natures');
 
 const app = express();
 const backPort = process.env.PORT;
 
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connection: ${err.stack}`);
-  } else {
-    console.log(`connected to database with threadId: ${connection.threadId}`);
-  }
-});
-
-app.use(express.json);
+app.use(express.json());
 app.use(cors());
 
-app.listen(connection.backPort, () => {
+app.use('/pokemons', pokemonsRoutes);
+app.use('/moves', movesRoutes);
+app.use('/abilities', abilitiesRoutes);
+app.use('/items', itemsRoutes);
+app.use('/types', typesRoutes);
+app.use('/natures', naturesRoutes);
+
+app.use('/', (req, res) => {
+  res.status(404).send(`Route not found: ${req.method} ${req.url}`);
+});
+
+app.listen(backPort, () => {
   console.log(`You are now connected on port ${backPort}`);
 });
